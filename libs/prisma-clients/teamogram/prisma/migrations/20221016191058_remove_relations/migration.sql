1fe5929813ab57c8d3a56c6e75620af83d754603
akaspranov <kaspergreen@gmail.com>
2022-10-16 22:12:21 +0300

    убрал релейшены у моделей

---
+++ libs/prisma-clients/teamogram/prisma/migrations/20221016191058_remove_relations/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE `Score` DROP FOREIGN KEY `Score_givingTelegramUserId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `Score` DROP FOREIGN KEY `Score_targetTelegramUserId_fkey`;
