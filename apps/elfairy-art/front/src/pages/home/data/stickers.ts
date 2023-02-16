5b11d79771980d319114635fea139b2f92e52c71
akaspranov <kaspergreen@gmail.com>
2023-02-16 19:10:58 +0300

    elfairy.art только чёткие стикеры

--- apps/elfairy-art/front/src/pages/home/data/stickers.ts
+++ apps/elfairy-art/front/src/pages/home/data/stickers.ts
@@ -3,62 +3,21 @@ import anahata_mandala_center_image from '../images/anahata_center.png'
 import circus_mandala_image from '../images/circus.png'
 import tira_image from '../images/tira_zelyonoe_colnce.png'
 import tira_image_small from '../images/small/tira_zelyonoe_colnce.png'
-import flower_of_life_image from '../images/flower_of_life.png'
-import flower_of_life_breath_image from '../images/flower_of_life_breath.png'
-import flower_of_sun_image from '../images/flower_of_sun.png'
-import flower_off_life_little_image from '../images/flower_off_life_little.png'
-import peach_circus_image from '../images/peach_circus.png'
-import rose_of_wings_image from '../images/rose_of_wings.png'
 import astra_portal_image from '../images/astra_portal.png'
-import carpet_plane_image from '../images/carpet_plane.png'
-import alatyr_sun_image from '../images/alatyr_sun.png'
 import metatrone_cube_image from '../images/metatrone_cube.png'
 import om_friendsheep_image from '../images/om_friendship.png'
 import om_friendsheep_center_image from '../images/om_friendship_center.png'
 import anahata_mandala_image_small from '../images/small/anahata.png'
 import circus_mandala_image_small from '../images/small/circus.png'
-import flower_of_life_image_small from '../images/small/flower_of_life.png'
-import flower_of_life_breath_image_small from '../images/small/flower_of_life_breath.png'
-import flower_of_sun_image_small from '../images/small/flower_of_sun.png'
-import flower_off_life_little_image_small from '../images/small/flower_off_life_little.png'
-import peach_circus_image_small from '../images/small/peach_circus.png'
-import rose_of_wings_image_small from '../images/small/rose_of_wings.png'
 import sun_dragonflies_small from '../images/small/sun_dragonflies.png'
 import sun_dragonflies_part_small from '../images/small/sun_dragonflies_part.png'
 import space_avoska_image_small from '../images/small/space_avoska.png'
 import space_avoska_image from '../images/space_avoska.png'
 import astra_portal_image_small from '../images/small/astra_portal.png'
-import carpet_plane_image_small from '../images/small/carpet_plane.png'
-import alatyr_sun_image_small from '../images/small/alatyr_sun.png'
 import metatrone_cube_image_small from '../images/small/metatrone_cube.png'
 import om_friendsheep_image_small from '../images/small/om_friendship.png'
 import sun_dragonflies from '../images/sun_dragonflies.png'
 import sun_dragonflies_part from '../images/sun_dragonflies_part.png'
-/* import magnet_1 from '../images/magnet_1.png'
-import magnet_1_small from '../images/small/magnet_1.png'
-import magnet_2 from '../images/magnet_2.png'
-import magnet_2_small from '../images/small/magnet_2.png'
-import magnet_3 from '../images/magnet_3.png'
-import magnet_3_small from '../images/small/magnet_3.png'
-import magnet_4 from '../images/magnet_4.png'
-import magnet_4_small from '../images/small/magnet_4.png'
-import magnet_5 from '../images/magnet_5.png'
-import magnet_5_small from '../images/small/magnet_5.png'
-import magnet_6 from '../images/magnet_6.png'
-import magnet_6_small from '../images/small/magnet_6.png'
-import magnet_7 from '../images/magnet_7.png'
-import magnet_7_small from '../images/small/magnet_7.png'
-import magnet_8 from '../images/magnet_8.png'
-import magnet_8_small from '../images/small/magnet_8.png'
-import magnet_9 from '../images/magnet_9.png'
-import magnet_9_small from '../images/small/magnet_9.png'
-import magnet_10 from '../images/magnet_10.png'
-import magnet_10_small from '../images/small/magnet_10.png'
-import magnet_11 from '../images/magnet_11.png'
-import magnet_11_small from '../images/small/magnet_11.png'
-import magnet_12 from '../images/magnet_12.png'
-import magnet_12_small from '../images/small/magnet_12.png'
-*/
 import square_15x15_4 from '../images/square_15x15_4.png'
 import square_15x15_4_small from '../images/small/square_15x15_4.png'
 import square_15x15_4_center from '../images/square_15x15_4_center.png'
@@ -95,24 +54,6 @@ export const stickers = {
     default_effect_index: 0,
     name: 'Цирк'
   },
-  carpet_plane: {
-    image_url: carpet_plane_image,
-    image_small_url: carpet_plane_image_small,
-    default_effect_index: 1,
-    name: 'Ковёр Самолёт'
-  },
-  alatyr_sun: {
-    image_url: alatyr_sun_image,
-    image_small_url: alatyr_sun_image_small,
-    default_effect_index: 0,
-    name: 'Алатырь Солнце'
-  },
-  flower_of_life: {
-    image_url: flower_of_life_image,
-    image_small_url: flower_of_life_image_small,
-    default_effect_index: 1,
-    name: 'Цветок Жизни'
-  },
   sun_dragonflies: {
     image_url: sun_dragonflies,
     image_small_url: sun_dragonflies_small,
@@ -136,35 +77,6 @@ export const stickers = {
     default_effect_index: 0,
     name: 'Космическая Авоська'
   },
-  peach_circus: {
-    image_url: peach_circus_image,
-    image_small_url: peach_circus_image_small,
-    name: 'Персиковый Цирк'
-  },
-  flower_of_life_breath: {
-    image_url: flower_of_life_breath_image,
-    image_small_url: flower_of_life_breath_image_small,
-    name: 'Дыхание Цветка Жизни',
-    default_effect_index: 4
-  },
-  flower_of_sun: {
-    image_url: flower_of_sun_image,
-    image_small_url: flower_of_sun_image_small,
-    default_effect_index: 3,
-    name: 'Цветок Солнца'
-  },
-  flower_off_life_little: {
-    image_url: flower_off_life_little_image,
-    image_small_url: flower_off_life_little_image_small,
-    name: 'Маленький Цветок Жизни'
-  },
-  rose_of_wings: {
-    image_url: rose_of_wings_image,
-    image_small_url: rose_of_wings_image_small,
-    name: 'Роза Ветров',
-    default_effect_index: 1
-
-  },
   square_15x15_1: {
     image_url: square_15x15_1,
     image_small_url: square_15x15_1_small,
@@ -181,89 +93,4 @@ export const stickers = {
     default_effect_index: 0,
     square: true
   },
-  /*
-  magnet_1: {
-    image_url: magnet_1,
-    image_small_url: magnet_1_small,
-    name: 'Магнит №1',
-    default_effect_index: 1
-
-  },
-  magnet_2: {
-    image_url: magnet_2,
-    image_small_url: magnet_2_small,
-    name: 'Магнит №2',
-    default_effect_index: 1
-
-  },
-  magnet_3: {
-    image_url: magnet_3,
-    image_small_url: magnet_3_small,
-    name: 'Магнит №3',
-    default_effect_index: 1
-
-  },
-  magnet_4: {
-    image_url: magnet_4,
-    image_small_url: magnet_4_small,
-    name: 'Магнит №4',
-    default_effect_index: 1
-
-  },
-  magnet_5: {
-    image_url: magnet_5,
-    image_small_url: magnet_5_small,
-    name: 'Магнит №5',
-    default_effect_index: 1
-
-  },
-  magnet_6: {
-    image_url: magnet_6,
-    image_small_url: magnet_6_small,
-    name: 'Магнит №6',
-    default_effect_index: 1
-
-  },
-  magnet_7: {
-    image_url: magnet_7,
-    image_small_url: magnet_7_small,
-    name: 'Магнит №7',
-    default_effect_index: 1
-
-  },
-  magnet_8: {
-    image_url: magnet_8,
-    image_small_url: magnet_8_small,
-    name: 'Магнит №8',
-    default_effect_index: 1
-
-  },
-  magnet_9: {
-    image_url: magnet_9,
-    image_small_url: magnet_9_small,
-    name: 'Магнит №9',
-    default_effect_index: 1
-
-  },
-  magnet_10: {
-    image_url: magnet_10,
-    image_small_url: magnet_10_small,
-    name: 'Магнит №10',
-    default_effect_index: 1
-
-  },
-  magnet_11: {
-    image_url: magnet_11,
-    image_small_url: magnet_11_small,
-    name: 'Магнит №11',
-    default_effect_index: 1
-
-  },
-  magnet_12: {
-    image_url: magnet_12,
-    image_small_url: magnet_12_small,
-    name: 'Магнит №12',
-    default_effect_index: 1
-  }
-   */
 }
