2722e93726305e7ff4ced84f6eab950f70475974
akaspranov <kaspergreen@gmail.com>
2023-03-04 13:00:15 +0530

    добавил старт для видео-кодера

--- apps/video/coder/src/main.ts
+++ apps/video/coder/src/main.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+process.env.FLUENTFFMPEG_COV = false
+//console.log(process.env)
+import fluentFfmpeg from 'fluent-ffmpeg'
+const ffmpegCommand = fluentFfmpeg()
+
+console.log('Hello World!');
+
+
+
+export function stop() {
+  return 0
+}
+
