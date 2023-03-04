2c16911be2374255f4340c14debf930d9e32ee99
akaspranov <kaspergreen@gmail.com>
2023-03-05 01:15:10 +0530

    мандала прайс-бот апдейт

--- apps/mandala-price-bot/src/main.ts
+++ apps/mandala-price-bot/src/main.ts
@@ -1,24 +1,65 @@
 import {config} from "dotenv";
+
 config()
-import {Telegraf} from 'telegraf';
+import {Telegraf, Markup} from 'telegraf';
 import {MandalaPrice} from "@cusp/mandala-price";
 
+const simpleSizes = [15, 20, 30, 40, 50, 60, 80, 90]
 const bot = new Telegraf(process.env.MANDALA_PRICE_BOT_TOKEN)
 bot.help((ctx) => ctx.reply('Тут нужно бы про команды. Например /me — её нужно писать с текстом'))
 bot.on('message', (ctx) => {
-  const msg: {text?: string} = ctx.message as unknown as any
+  const msg: { text?: string } = ctx.message as unknown as any
   const text = msg?.text || ""
-  const matchNumber = text.match(/^\d{1,4}$/)
-  const matchCentimeters = text.match(/^(\d{1,3})см$/)
-  if(matchNumber || matchCentimeters) {
-    const millimeters = Number(matchNumber?.[0]) || Number(matchCentimeters?.[1]) * 10
-    const price = MandalaPrice.calculate(millimeters)
-    const formattedPrice = new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(price)
-    ctx.reply(`${formattedPrice}`)
+  const matchCentimeters = text.match(/^\d{1,4}$/)
+  if (!matchCentimeters) {
+    ctx.reply(`
+
+Небольшая мандала-магнит, диаметром 80 миллиметров, стоит 500 рублей. Она состоит из 50 квадратных сантиметров.
+Стоимость одного квадратного сантиметра, с учётом работы и материалов, составляет 10 рублей.
+
+Мандалы большого размера редки, сложны и долги в изготовлении, требуют специальных инструментов, подготовки и терпения.
+Большие мандалы имеют наценку пропорциональную размеру, например двухметровая мандала будет стоить 20 рублей за квадратный сантиметр,
+так как при таком размере сложность как минимум удваивается, а само изготовление и работа требуют капитальных вложений средств и времени.
+
+Введите размер мандалы в сантиметрах или выберите из типовых размеров и получите примерную стоимость.`, {
+      parse_mode: 'HTML',
+      ...Markup.inlineKeyboard([
+        ...simpleSizes.map((size) => {
+          return Markup.button.callback(size + 'см', `size${size}`)
+        })
+      ])
+    })
+
   } else {
-    ctx.reply('Введите радиус мандалы в миллиметрах и получите примерную стоимость.')
+    ctx.reply(replyBySize(Number(matchCentimeters)))
   }
 })
+
+function replyBySize(size: number) {
+  const millimeters = size * 10
+  const square = Math.PI * (millimeters / 10 / 2) ** 2
+  const price = MandalaPrice.calculate(millimeters)
+  const formattedPrice = new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB'}).format(price)
+  return (
+    `
+Мандала со стороной ${millimeters / 10}см займёт ${Math.ceil(square)}см²
+
+(это примерно ${Math.ceil(square / 50)} шт. в пересчёте на магниты со стороной 8см)
+
+будет стоить примерно ${formattedPrice}₽
+
+`
+  )
+}
+
+simpleSizes.forEach((size) => {
+  bot.action(`size${size}`, async (ctx) => {
+    await ctx.answerCbQuery()
+    await ctx.reply(replyBySize(size))
+  })
+})
+
+
 bot.launch().then(() => {
   console.log('Бот запустился!')
 })
